f5c6d07b64a026349b0fa6386483b8614412bbf2
Wasis Haryo Sasoko <yokowasis@gmail.com>
2017-07-26 23:25:42 +0800

    p

--- usr/local/cwpsrv/htdocs/resources/client/include/3rdparty.php
+++ usr/local/cwpsrv/htdocs/resources/client/include/3rdparty.php
@@ -1,5 +1,5 @@
 <li><a href="index.php?module=installer_wordpress"><span class="icon16 icomoon-icon-arrow-right-3"></span>Wordpress Installer</a></li>
-<li><a href="https://www.cloudflare.com/a/overview" target="_blank"><span class="icon16 icomoon-icon-arrow-right-3"></span>Cloudflare</a></li>
+<li><a href="index.php?module=cloudflare"><span class="icon16 icomoon-icon-arrow-right-3"></span>Cloudflare</a></li>
 
 <script type="text/javascript">
 	jQuery(document).ready(function($) {
--- usr/local/cwpsrv/htdocs/resources/client/modules/cloudflare.php
+++ usr/local/cwpsrv/htdocs/resources/client/modules/cloudflare.php
@@ -1,1 +1,36 @@
-<iframe src="https://google.com" height="500" width="800"></iframe>
+<?php
+	
+	function cf_listrecords($zoneid,$email,$key,$type="A"){
+		$url = 'https://api.cloudflare.com/client/v4/zones/'.$zoneid.'/dns_records/?type='.$type.' ';
+		$output = shell_exec('
+		curl -X GET "'.$url.'" -H "X-Auth-Email: '.$email.'" -H "X-Auth-Key: '.$key.'" -H "Content-Type: application/json"
+		');
+		return json_decode($output);
+	}
+
+
+
+$zoneid = '141a4b1806b1c11f3b4d0cdb2e11c967';
+$email = 'yokowasis@gmail.com';
+$key = '52115cca3a7b3a4ad11462e9b67b9876e88f6';
+
+	$cf = cf_listrecords($zoneid,$email,$key);
+	//print_r ($cf);
+	?>
+		<table>
+			<tr>
+				<td>Type</td>
+				<td>Name</td>
+				<td>Value</td>
+				<td>Proxied</td>
+			</tr>
+			<?php for ($i=0;$i<count($cf->result);$i++) : ?>
+			<tr>
+				<td><?php echo ($cf->result[$i]->type) ?></td>
+				<td><?php echo ($cf->result[$i]->name) ?></td>
+				<td><?php echo ($cf->result[$i]->content) ?></td>
+				<td><?php echo ($cf->result[$i]->proxied) ?></td>
+			</tr>
+			<?php endfor; ?>
+		</table>
+	<?php
